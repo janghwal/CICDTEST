68eb30ef0e95bacea480a70e0642a676d999b065
janghwal <jhp337037@gmail.com>
2025-05-23 20:32:00 +0900

    기본 기능 DB X

---
+++ src/main/java/com/example/cicdtest/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.example.cicdtest.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.cicdtest.service.TestService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/test")
+@RequiredArgsConstructor
+public class TestController {
+	private final TestService testService;
+
+	@PostMapping
+	public ResponseEntity<String> controllerTest(
+		@RequestParam String str
+	) {
+		String result = testService.serviceTest(str);
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+
+}
